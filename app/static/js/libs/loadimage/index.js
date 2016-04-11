2c82d1e029957b62b2f822bfdab4a1d7c1000311
Bill Burton <bburton@ripplefly.com>
2016-04-11 18:48:33 -0400

    revised requireJS

---
+++ app/static/js/libs/loadimage/index.js
@@ -0,0 +1,9 @@
+define(function(require, exports, module) {
+    module.exports = require('load-image');
+    require('libs/loadimage/load-image-exif');
+    require('libs/loadimage/load-image-exif-map');
+    require('libs/loadimage/load-image-meta');
+    require('libs/loadimage/load-image-orientation');
+});
+
+
