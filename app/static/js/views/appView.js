76d3dea7056693dc39aac3374545c33e8eda6e96
Bill Burton <bburton@ripplefly.com>
2016-04-27 14:51:18 -0400

    added garbage collection

---
+++ app/static/js/views/appView.js
@@ -0,0 +1,55 @@
+define(['jquery'],
+    function($){
+        return function(view, options) {
+            options = options || {};
+            if (view.el.id == 'main-image') {
+                if (this.mainView) {
+                    this.mainView.close();
+                }
+                this.mainView = view;
+                if (options.render) {
+                    if (options.entity == 'photo'){
+                        if (this.archiveView) {
+                            this.archiveView.unrender();
+                            this.archiveView.close();
+                        }
+                    }
+                    this.mainView.render(options);
+                    $('#photo-main').html(this.mainView.el);
+                } else {
+                    this.mainView.attachToView();               
+                }
+            } else if (view.el.id == 'links') {
+                if (this.archiveView) {
+                    this.archiveView.unrender();
+                    this.archiveView.close();
+                }
+                this.archiveView = view;
+                if (options.render) {
+                    this.archiveView.render(options);
+                    $('#photo-archives').html(this.archiveView.el);
+                } else {
+                    this.archiveView.attachToView();
+                }
+            } else if (view.el.id == 'header') {
+                if (this.headerView) {
+                    this.headerView.close();
+                }
+                this.headerView = view;
+                if (options.render) {
+                    this.headerView.render(options);
+                    $('header').html(this.headerView.el);
+                }
+            } else if (view.el.id == 'navbar') {
+                if (this.navView) {
+                    this.navView.close();
+                }
+                this.navView = view;
+                if (options.render) {
+                    this.navView.render(options);
+                    $('nav').html(this.navView.el);
+                }
+            }
+        };
+    }
+);
