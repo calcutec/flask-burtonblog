d3cab7af6db42a1358eeb685e4ca41b210a57ff2
Bill Burton <bburton@ripplefly.com>
2016-05-04 22:25:38 -0400

    updated structure

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,8 @@ from flask.ext.assets import Environment, Bundle
 from config import ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD, SQLALCHEMY_DATABASE_URI
 from flask_wtf.csrf import CsrfProtect
 
+staticdirectory = "static"
+
 
 class MyFlask(Flask):
     @property
@@ -21,11 +23,11 @@ class MyFlask(Flask):
         self.config['STATIC_FOLDER'] = value
 
 # Now these are equivalent:
-app = Flask(__name__, static_folder='static')
-app.config['STATIC_FOLDER'] = 'static'
+app = Flask(__name__, static_folder=staticdirectory)
+app.config['STATIC_FOLDER'] = staticdirectory
 
 base_dir = os.path.dirname(os.path.realpath(__file__))
-app.jinja_loader = FileSystemLoader(os.path.join(base_dir, 'static', 'templates'))
+app.jinja_loader = FileSystemLoader(os.path.join(base_dir, staticdirectory, 'templates'))
 app.jinja_env.globals['momentjs'] = momentjs
 app.config.from_object('config')
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
