c4e53271beba1713062cdc896bfd6b28ea22cbbc
Bill Burton <bburton@ripplefly.com>
2016-06-13 14:46:42 -0400

    fixed file upload

--- app/static/js/views/photoTextFormView.js
+++ app/static/js/views/photoTextFormView.js
@@ -23,20 +23,7 @@ define(['jquery', 'backbone', 'ds', 'nunjucks', 'models/photoModel', 'views/appV
                     type: 'POST',
                     success: function (model) {
                         DS.inject('photo', model);
-                        DS.set({'route': null, 'collection': null, 'category': null, 'entity': null, 'nickname': null,
-                            'authenticated': window.env.globals.current_user.is_authenticated(), 'count': null,
-                            'postId': null, 'template': null, 'render': true });
-                        DS.set('collection', DS.getAll('photo'));
-                        DS.set('counts', self.$el.getCounts(DS.get('collection')));
-                        DS.set('entity', 'photo');
-                        DS.set('postId', model.id);
-                        DS.set('route', '/photos/' + DS.get('postId') + '/');
-                        DS.set('usernickname', window.env.globals.current_user.usernickname);
-                        Backbone.history.navigate(DS.get('route'), {trigger: false});
-                        DS.set('render', true);
-                        AppView(new HeaderView({id: 'header'}));
-                        AppView(new NavView({id: 'navbar'}));
-                        AppView(new DetailView({id: 'main-view', model: model}));
+                        $( ".fa-picture-o" ).trigger( "click" );
                     },
                     error: function () {
                         alert('your poem did not save properly..')
