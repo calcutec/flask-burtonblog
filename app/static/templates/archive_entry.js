7c7c0c35dc0249970f4121c31e7ae2f230d45199
Bill Burton <bburton@ripplefly.com>
2016-06-16 17:18:18 -0400

    fixing detail link

--- app/static/templates/archive_entry.js
+++ app/static/templates/archive_entry.js
@@ -58,7 +58,7 @@ output += "\n            <span class=\"gallery\">\n                <i class=\"fa
 }
 output += "\n</span>\n";
 if(runtime.contextOrFrameLookup(context, frame, "assets")) {
-output += "\n        <a class=\"detail-link\" data-id=\"";
+output += "\n        <a onclick=\"\" class=\"detail-link\" data-id=\"";
 output += runtime.suppressValue(runtime.memberLookup((runtime.contextOrFrameLookup(context, frame, "post")),"id"), env.opts.autoescape);
 output += "\" href=\"/photos/";
 output += runtime.suppressValue(runtime.memberLookup((runtime.contextOrFrameLookup(context, frame, "post")),"id"), env.opts.autoescape);
